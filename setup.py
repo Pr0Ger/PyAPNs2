f2d5f9e5d06657b7c4c4bc7d6f30284bedf9cecd
Sergey Petrov <git@pr0ger.org>
2016-01-28 12:21:29 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='apns2',
+    version='0.1.0',
+    packages=['apns2'],
+    url='',
+    license='MIT',
+    author='Petrov Sergey',
+    author_email='me@pr0ger.prg',
+    description='A python library for interacting with the Apple Push Notification Service via HTTP/2 protocol'
+)

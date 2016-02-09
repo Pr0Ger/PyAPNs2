61f38d391e1b4890a460edb49122cafb5c38e69f
Sergey Petrov <git@pr0ger.org>
2016-02-09 15:39:29 +0300

    setup.py adjustements

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 from distutils.core import setup
 
 setup(
@@ -5,9 +7,17 @@ setup(
     version='0.1.0',
     packages=['apns2'],
     requires=['hyper'],
-    url='',
+    url='https://github.com/Pr0Ger/PyAPNs2',
     license='MIT',
     author='Sergey Petrov',
     author_email='me@pr0ger.prg',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+    ],
     description='A python library for interacting with the Apple Push Notification Service via HTTP/2 protocol'
 )

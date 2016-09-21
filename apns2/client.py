a4ede7a71e2dbac02e6100ca918225c80125d7a8
Natalia Akentyeva <nt.aknt@gmail.com>
2016-09-21 16:33:12 +0300

    some changes and fixes for Sports use
    - add send_notifications for sending several tokens
    - remove send_notification

--- apns2/client.py
+++ apns2/client.py
@@ -1,42 +1,48 @@
-from enum import Enum
 from json import dumps
 
-import json
-from hyper import HTTP20Connection
-from hyper.tls import init_context
+from tornado import gen
+from http2 import SimpleAsyncHTTP2Client
+from functools import partial
 
-from apns2.errors import exception_class_for_reason
-
-
-class NotificationPriority(Enum):
-    Immediate = '10'
-    Delayed = '5'
+NOTIFICATION_PRIORITY = dict(immediate='10', delayed='5')
 
 
 class APNsClient(object):
-    def __init__(self, cert_file, use_sandbox=False, use_alternative_port=False, proto=None):
+    def __init__(self, cert_file, key_file, use_sandbox=False, use_alternative_port=False, proto=None, http_client_key=None):
         server = 'api.development.push.apple.com' if use_sandbox else 'api.push.apple.com'
         port = 2197 if use_alternative_port else 443
-        ssl_context = init_context()
-        ssl_context.load_cert_chain(cert_file)
-        self.__connection = HTTP20Connection(server, port, ssl_context=ssl_context, force_proto=proto or 'h2')
+        cert_options = dict(validate_cert=True, client_cert=cert_file, client_key=key_file)
+        self.__http_client = SimpleAsyncHTTP2Client(host=server, port=port, secure=True, cert_options=cert_options, enable_push=True, connect_timeout=2, max_streams=20, http_client_key=http_client_key)
+        self.__url_pattern = '/3/device/{token}'
+        self.cert_file = cert_file
 
-    def send_notification(self, token_hex, notification, priority=NotificationPriority.Immediate, topic=None, expiration=None):
+    def __repr__(self):
+        return "APNSClient: {}".format(self.cert_file)
+
+    @gen.coroutine
+    def send_notifications(self, tokens, notification, priority=NOTIFICATION_PRIORITY['immediate'], topic=None, expiration=None, cb=None):
         json_payload = dumps(notification.dict(), ensure_ascii=False, separators=(',', ':')).encode('utf-8')
-
         headers = {
-            'apns-priority': priority.value
+            'apns-priority': priority
         }
         if topic:
             headers['apns-topic'] = topic
 
         if expiration is not None:
             headers['apns-expiration'] = "%d" % expiration
+        
+        futures = []
 
-        url = '/3/device/{}'.format(token_hex)
-        stream_id = self.__connection.request('POST', url, json_payload, headers)
-        resp = self.__connection.get_response(stream_id)
-        if resp.status != 200:
-            raw_data = resp.read().decode('utf-8')
-            data = json.loads(raw_data)
-            raise exception_class_for_reason(data['reason'])
+        for token in tokens:
+            url = self.__url_pattern.format(token=token)
+            if cb:
+                callback = partial(cb, token=token)
+            else:
+                callback = None
+            print('Headers: %s' % headers)
+            print('Payload: %s' % json_payload)
+            print('Url: %s' % url)
+            future = self.__http_client.fetch(url, method='POST', body=json_payload, headers=headers, callback=callback, raise_error=False)
+            futures.append(future)
+
+        yield futures

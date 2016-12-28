5664206de141a119d0fff92710313a6c6709aa6b
Yannic Bonenberger <contact@yannic-bonenberger.com>
2016-12-28 21:25:05 +0100

    Fix issues

--- apns2/client.py
+++ apns2/client.py
@@ -1,7 +1,7 @@
 from enum import Enum
 
 from json import loads, dumps
-from hyper import HTTP20Connection
+from hyper import HTTP20Connection as HTTP2
 from hyper.tls import init_context
 
 from apns2.errors import exception_class_for_reason
@@ -9,7 +9,7 @@ from apns2.payload import Payload
 
 # const
 SERVER_ADDR = 'api.push.apple.com'
-SERVER_ADDR_DEV = 'api.development.push.apple.com'
+SERVER_ADDR_SANDBOX = 'api.development.push.apple.com'
 
 PORT = 443
 ALTERNATIVE_PORT = 2197
@@ -22,28 +22,42 @@ class NotificationPriority(Enum):
 
 class APNsClient(object):
 
-    def __init__(self, cert_file, use_sandbox=False, use_alternative_port=False, proto=None, json_encoder=None):
-        self.__json_encoder = json_encoder
+    def __init__(self, cert_file, **kargs):
+        self.__encoder = None
+        if 'json_encoder' in kargs:
+            self.__encoder = kargs['json_encoder']
+
+        addr = SERVER_ADDR
+        if 'use_sandbox' in kargs and kargs['use_sandbox']:
+            addr = SERVER_ADDR_SANDBOX
+
+        port = PORT
+        if 'use_alternative_port' in kargs and kargs['use_alternative_port']:
+            port = ALTERNATIVE_PORT
+
+        proto = None
+        if 'proto' in kargs:
+            proto = kargs['proto']
 
         self.__connection = None
         self.__connection_parameters = {
-            'server': SERVER_ADDR_DEV if use_sandbox else SERVER_ADDR,
-            'port': ALTERNATIVE_PORT if use_alternative_port else PORT,
+            'server': addr,
+            'port': port,
             'ssl_context': {
                 'cert_file': cert_file
             },
             'proto': proto or 'h2'
         }
 
-    def send_notification(self, token_hex, notification, priority=NotificationPriority.Immediate, topic=None, expiration=None):
+    def send_notification(self, token_hex, notification, **kargs):
         payload = notification
         if isinstance(notification, Payload):
             payload = notification.dict()
 
-        json_payload = dumps(payload, cls=self.__json_encoder, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
-        headers = get_headers(priority, topic, expiration)
+        json = dumps(payload, cls=self.__encoder, ensure_ascii=False, separators=(',', ':'))
+        headers = get_headers(**kargs)
 
-        with self.__send_request(token_hex, json_payload, headers) as resp:
+        with self.__send_request(token, json.encode('utf-8'), headers) as resp:
             raw_data = resp.read().decode('utf-8')
 
             if resp.status != 200:
@@ -52,29 +66,32 @@ class APNsClient(object):
 
     @staticmethod
     def get_headers(priority, topic=None, expiration=None):
+        if priority not in kargs:
+            kargs['priority'] = NotificationPriority.Immediate
+
         headers = {
-            'apns-priority': priority.value
+            'apns-priority': kargs['priority'].value
         }
 
-        if topic:
-            headers['apns-topic'] = topic
+        if 'topic' in kargs and kargs['topic']:
+            headers['apns-topic'] = kargs['topic']
 
-        if expiration is not None:
-            headers['apns-expiration'] = "%d" % expiration
+        if 'expiration' in kargs and kargs['expiration'] is not None:
+            headers['apns-expiration'] = "%d" % kargs['expiration']
 
         return headers
 
     def __send_request(self, token, payload, headers):
         if not self.__connection:  # Lazy Connecting
-            server = self.__connection_parameters['server']
+            s = self.__connection_parameters['server']
             port = self.__connection_parameters['port']
             cert = self.__connection_parameters['ssl_context']['cert_file']
-            proto = self.__connection_parameters['proto']
+            pr = self.__connection_parameters['proto']
 
-            ssl_context = init_context()
-            ssl_context.load_cert_chain(cert)
+            ssl = init_context()
+            ssl.load_cert_chain(cert)
 
-            self.__connection = HTTP20Connection(server, port, ssl_context=ssl_context, force_proto=proto)
+            self.__connection = HTTP2(s, port, ssl_context=ssl, force_proto=pr)
 
         url = '/3/device/{}'.format(token)
         stream_id = self.__connection.request('POST', url, payload, headers)

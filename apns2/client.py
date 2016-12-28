e41e8d41fac305e13bd348c530d92b6829f0cacd
Yannic Bonenberger <contact@yannic-bonenberger.com>
2016-12-28 19:08:36 +0100

    Send custom payload without subclassing apns2.Payload

--- apns2/client.py
+++ apns2/client.py
@@ -1,11 +1,11 @@
 from enum import Enum
-from json import dumps
 
 import json
 from hyper import HTTP20Connection
 from hyper.tls import init_context
 
 from apns2.errors import exception_class_for_reason
+from apns2.payload import Payload
 
 
 class NotificationPriority(Enum):
@@ -23,12 +23,16 @@ class APNsClient(object):
         self.__json_encoder = json_encoder
 
     def send_notification(self, token_hex, notification, priority=NotificationPriority.Immediate, topic=None, expiration=None):
-        json_payload = dumps(notification.dict(), cls=self.__json_encoder, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
+        payload = notification
+        if isinstance(notification, Payload):
+            payload = notification.dict()
+
+        json_payload = json.dumps(payload, cls=self.__json_encoder, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
 
         headers = {
             'apns-priority': priority.value
         }
-        if topic:
+        if topic is not None:
             headers['apns-topic'] = topic
 
         if expiration is not None:
@@ -36,9 +40,10 @@ class APNsClient(object):
 
         url = '/3/device/{}'.format(token_hex)
         stream_id = self.__connection.request('POST', url, json_payload, headers)
-        resp = self.__connection.get_response(stream_id)
-        with resp:
+
+        with self.__connection.get_response(stream_id) as resp:
+            raw_data = resp.read().decode('utf-8')
+
             if resp.status != 200:
-                raw_data = resp.read().decode('utf-8')
                 data = json.loads(raw_data)
                 raise exception_class_for_reason(data['reason'])

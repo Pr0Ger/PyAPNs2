b3d1b8d0799ea00ba2c8add71a24b73d6f68e361
Yannic Bonenberger <contact@yannic-bonenberger.com>
2016-12-28 20:22:09 +0100

    Fix codeclimate issues

--- apns2/client.py
+++ apns2/client.py
@@ -1,11 +1,18 @@
 from enum import Enum
 
-import json
+import loads, dumps
 from hyper import HTTP20Connection
 from hyper.tls import init_context
 
 from apns2.errors import exception_class_for_reason
 from apns2.payload import Payload
+
+# const
+SERVER_ADDR = 'api.push.apple.com'
+SERVER_ADDR_DEV = 'api.development.push.apple.com'
+
+PORT = 443
+ALTERNATIVE_PORT = 2197
 
 
 class NotificationPriority(Enum):
@@ -14,36 +21,59 @@ class NotificationPriority(Enum):
 
 
 class APNsClient(object):
+
     def __init__(self, cert_file, use_sandbox=False, use_alternative_port=False, proto=None, json_encoder=None):
-        server = 'api.development.push.apple.com' if use_sandbox else 'api.push.apple.com'
-        port = 2197 if use_alternative_port else 443
-        ssl_context = init_context()
-        ssl_context.load_cert_chain(cert_file)
-        self.__connection = HTTP20Connection(server, port, ssl_context=ssl_context, force_proto=proto or 'h2')
         self.__json_encoder = json_encoder
 
-    def send_notification(self, token_hex, notification, priority=NotificationPriority.Immediate, topic=None, expiration=None):
-        payload = notification
-        if isinstance(notification, Payload):
-            payload = notification.dict()
+        self.__connection = None
+        self.__connection_parameters = {
+            'server': SERVER_ADDR_DEV if use_sandbox else SERVER_ADDR,
+            'port': ALTERNATIVE_PORT if use_alternative_port else PORT,
+            'ssl_context': {
+                'cert_file': cert_file
+            },
+            'proto': proto or 'h2'
+        }
 
-        json_payload = json.dumps(payload, cls=self.__json_encoder, ensure_ascii=False, separators=(',', ':')).encode('utf-8')
+    def send_notification(self, token_hex, notification, priority=NotificationPriority.Immediate, topic=None, expiration=None):
+        payload = notification.dict() if isinstance(notification, Payload) else notification
+
+        json_payload = dumps(
+            payload, 
+            cls=self.__json_encoder, 
+            ensure_ascii=False, 
+            separators=(',', ':')
+            ).encode('utf-8')
 
         headers = {
             'apns-priority': priority.value
         }
-        if topic is not None:
+        if topic:
             headers['apns-topic'] = topic
 
-        if expiration is not None:
+        if expiration:
             headers['apns-expiration'] = "%d" % expiration
 
-        url = '/3/device/{}'.format(token_hex)
-        stream_id = self.__connection.request('POST', url, json_payload, headers)
-
-        with self.__connection.get_response(stream_id) as resp:
+        with self.__send_request(token_hex, json_payload, headers) as resp:
             raw_data = resp.read().decode('utf-8')
 
             if resp.status != 200:
-                data = json.loads(raw_data)
+                data = loads(raw_data)
                 raise exception_class_for_reason(data['reason'])
+
+    def __send_request(self, token, payload, headers):
+        #Lazy Connecting
+        if not self.__connection:
+            ssl_context = init_context()
+            ssl_context.load_cert_chain(cert_file)
+
+            self.__connection = HTTP20Connection(
+                self.__connection_parameters['server'], 
+                self.__connection_parameters['port'], 
+                ssl_context=ssl_context, 
+                self.__connection_parameters['proto']
+                )
+
+        url = '/3/device/{}'.format(token_hex)
+        stream_id = self.__connection.request('POST', url, json_payload, headers)
+        return self.__connection.get_response(stream_id)

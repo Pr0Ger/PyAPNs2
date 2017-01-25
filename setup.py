39644b6f0e24fc14a367e5a7943ee125f2c0fe4e
Sergey Petrov <git@pr0ger.org>
2017-01-25 17:27:21 +0300

    Remove 3.3 support

--- setup.py
+++ setup.py
@@ -22,9 +22,10 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2.7'
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6'
         'Topic :: Software Development :: Libraries',
     ],
     description='A python library for interacting with the Apple Push Notification Service via HTTP/2 protocol'

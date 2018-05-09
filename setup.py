6159383a8140924c4d6ef98248c62ed82d0026c5
Sergey Petrov <git@pr0ger.org>
2018-05-09 16:26:07 +0300

    version bump -> 0.4.1

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except ImportError:
 
 setup(
     name='apns2',
-    version='0.4.0',
+    version='0.4.1',
     packages=['apns2'],
     install_requires=dependencies,
     url='https://github.com/Pr0Ger/PyAPNs2',

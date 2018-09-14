004c74f0bd59804d91dfa1a4e4cd6e3833faec2b
aaronjheng <wentworth@outlook.com>
2018-09-14 16:28:48 +0800

    Use new sandbox address

--- apns2/client.py
+++ apns2/client.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 
 
 class APNsClient(object):
-    SANDBOX_SERVER = 'api.development.push.apple.com'
+    SANDBOX_SERVER = 'api.sandbox.push.apple.com'
     LIVE_SERVER = 'api.push.apple.com'
 
     DEFAULT_PORT = 443
